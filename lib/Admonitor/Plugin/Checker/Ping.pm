d8bfacb45845e50f5da7d6fd46bd2263cf641c56
Andy Beverley <andy@andybev.com>
2024-04-06 10:22:48 +0100

    Add docs for ping permissions

--- lib/Admonitor/Plugin/Checker/Ping.pm
+++ lib/Admonitor/Plugin/Checker/Ping.pm
@@ -16,6 +16,12 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 =cut
 
+#
+# The following is needed for this checker to work:
+#
+# echo "0 1000" > /proc/sys/net/ipv4/ping_group_range
+#
+
 package Admonitor::Plugin::Checker::Ping;
 
 use strict;
